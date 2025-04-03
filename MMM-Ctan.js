a75be40aa2a06b08033b59e33a2508d75d24f028
Infinitte <lorenzo.ortiz@gmail.com>
2025-04-03 21:33:04 +0200

    Artifacts

--- MMM-Ctan.js
+++ MMM-Ctan.js
@@ -1,72 +1,83 @@
 Module.register("MMM-Ctan", {
-  defaults: {
-parada: 1,
-      consorcio: 7,
-updateInterval: 300 // Every 5 minute
-  },
-  start: function() {
-      Log.info("Iniciando el módulo: " + this.name);
-      this.resultados = [];
-      this.getData();
-      this.scheduleUpdate();
-  },
-  getData: function() {
-      this.sendSocketNotification("GET_CTAN_DATA", {
-          parada: this.config.parada,
-          consorcio: this.config.consorcio
-      });
-  },
-  scheduleUpdate: function() {
-      setInterval(() => {
-          this.getData();
-      }, this.config.updateInterval*1000);
-  },
-  socketNotificationReceived: function(notification, payload) {
-      if (notification === "CTAN_DATA") {
-          if (payload && payload.servicios) {
-              this.resultados = payload.servicios.map(servicio => ({
-                  hora: servicio.servicio,
-                  nombre: servicio.nombre,
-                  destino: servicio.destino
-              }));
-          } else {
-              this.resultados = []; // Datos vacíos en caso de error o sin servicios
-          }
-          this.updateDom();
-      }
-  },
-  getDom: function() {
-      var wrapper = document.createElement("div");
-      wrapper.className = "MMM-ctan";
-      var titulo = document.createElement("header");
-      titulo.textContent = "Parada " + this.config.parada;
-      titulo.className = "module-header";
-      wrapper.appendChild(titulo);
-      if (this.resultados.length === 0) {
-          wrapper.innerHTML = "<p>Cargando...</p>";
-          return wrapper;
-      }
-      var tabla = document.createElement("table");
-      tabla.className = "small"; // Clase para el tipo de letra
-      var encabezado = tabla.createTHead();
-      var filaEncabezado = encabezado.insertRow();
-      var encabezados = ["Hora", "Nombre", "Destino"];
-      encabezados.forEach(texto => {
-          var th = document.createElement("th");
-          th.textContent = texto;
-          filaEncabezado.appendChild(th);
-      });
-      var cuerpoTabla = tabla.createTBody();
-      this.resultados.forEach(resultado => {
-          var fila = cuerpoTabla.insertRow();
-          var celdaLinea = fila.insertCell();
-          celdaLinea.textContent = resultado.nombre;
-          var celdaDestino = fila.insertCell();
-          celdaDestino.textContent = resultado.destino;
-          var celdaTiempo = fila.insertCell();
-          celdaTiempo.textContent = resultado.hora;
-      });
-      wrapper.appendChild(tabla);
-      return wrapper;
-  }
+    defaults: {
+	    parada: 1,
+        consorcio: 7,
+	    updateInterval: 300 // Every 5 minute
+    },
+    getStyles() {
+        return ["MMM-Ctan.css"]
+    },
+    start: function() {
+        Log.info("Iniciando el módulo: " + this.name);
+        this.resultados = [];
+        this.getData();
+        this.scheduleUpdate();
+    },
+    getData: function() {
+        this.sendSocketNotification("GET_CTAN_DATA", {
+            parada: this.config.parada,
+            consorcio: this.config.consorcio
+        });
+    },
+    scheduleUpdate: function() {
+        setInterval(() => {
+            this.getData();
+        }, this.config.updateInterval*1000);
+    },
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === "CTAN_DATA") {
+            if (payload && payload.servicios) {
+                this.resultados = payload.servicios.map(servicio => ({
+                    hora: servicio.servicio,
+                    nombre: servicio.nombre,
+                    destino: servicio.destino
+                }));
+            } else {
+                this.resultados = []; // Datos vacíos en caso de error o sin servicios
+            }
+            this.updateDom();
+        }
+    },
+    getDom: function() {
+        var wrapper = document.createElement("div");
+        wrapper.className = "MMM-ctan";
+        var titulo = document.createElement("header");
+        titulo.textContent = "Parada " + this.config.parada;
+        titulo.className = "module-header";
+        wrapper.appendChild(titulo);
+        if (this.resultados.length === 0) {
+            wrapper.innerHTML = "<p>Cargando...</p>";
+            return wrapper;
+        }
+        var tabla = document.createElement("table");
+        var encabezado = tabla.createTHead();
+        var filaEncabezado = encabezado.insertRow();
+        var encabezados = ["Nombre", "Destino", "Hora"];
+        encabezados.forEach(texto => {
+            var th = document.createElement("th");
+            th.textContent = texto;
+            filaEncabezado.appendChild(th);
+        });
+        var cuerpoTabla = tabla.createTBody();
+        this.resultados.forEach(resultado => {
+            var fila = cuerpoTabla.insertRow();
+            var celdaNombre = fila.insertCell();
+            var textoCompleto = resultado.nombre;
+            if (textoCompleto.length > 25) {
+                var indice = 0;
+                setInterval(() => {
+                    celdaNombre.textContent = textoCompleto.substring(indice, indice+25);
+                    indice = (indice + 1) % (textoCompleto.length + 1);
+                }, 200); // Ajusta la velocidad del carrusel según sea necesario
+            } else {
+                celdaNombre.textContent = textoCompleto;
+            }
+            var celdaDestino = fila.insertCell();
+            celdaDestino.textContent = resultado.destino;
+            var celdaTiempo = fila.insertCell();
+            celdaTiempo.textContent = resultado.hora;
+        });
+        wrapper.appendChild(tabla);
+        return wrapper;
+    }
 });
--- README.md
+++ README.md
@@ -1,71 +1,70 @@
-# MMM-Template
-Use this template for creating new MagicMirror² modules.
+# MMM-Ctan
+This [MagicMirror](https://github.com/MichMich/MagicMirror) module displays information about estimations for bus stops in Consorcio de Transportes de Andalucia. The service is provided by EMT Valencia. Supports Spanish (es).
 
-See the [wiki page](https://github.com/Dennis-Rosenbaum/MMM-Template/wiki) for an in depth overview of how to get started.
+![Example of MMM-Ctan](./example_1.png)
 
-# MMM-Template
-
-![Example of MMM-Template](./example_1.png)
-
-[Module description]
+## Dependencies
+- An installation of [MagicMirror<sup>2</sup>](https://github.com/MichMich/MagicMirror)
 
 ## Installation
 
-### Install
-
-In your terminal, go to your [MagicMirror²][mm] Module folder and clone MMM-Template:
-
-```bash
-cd ~/MagicMirror/modules
-git clone [GitHub url]
+Navigate into your MagicMirror's `modules` folder:
 ```
+cd ~/MagicMirror/modules
+```
+
+Clone this repository:
+```
+git clone https://github.com/Infinitte/MMM-Ctan.git
+```
+
+Navigate Navigate to the new `MMM-Ctan` folder and install the node dependencies.
+
+Clone this repository:
+```
+npm install request
+```
+
+Configure the module in your `config.js` file.
 
 ### Update
 
 ```bash
-cd ~/MagicMirror/modules/MMM-Template
+cd ~/MagicMirror/modules/MMM-Ctan
 git pull
 ```
 
+## Find your stop number
+
+
+
 ## Using the module
 
-To use this module, add it to the modules array in the `config/config.js` file:
+To use this module, add it to the modules array in the `config/config.js` file. 
 
-```js
-    {
-        module: 'MMM-Template',
-        position: 'lower_third'
-    },
-```
-
-Or you could use all the options:
-
-```js
-    {
-        module: 'MMM-Template',
-        position: 'lower_third',
-        config: {
-            exampleContent: 'Welcome world'
-        }
-    },
+```javascript
+modules: [
+  {
+    module: 'MMM-EMTValencia',
+    position: 'top_left',
+    header: 'My bus stop',
+    config: {
+      stopNumber: 683,
+      stopLine: null,
+      updateInterval: 10
+    }
+  },
+]
 ```
 
 ## Configuration options
 
-Option|Possible values|Default|Description
-------|------|------|-----------
-`exampleContent`|`string`|not available|The content to show on the page
+The following properties can be configured:
 
-## Sending notifications to the module
-
-Notification|Description
-------|-----------
-`TEMPLATE_RANDOM_TEXT`|Payload must contain the text that needs to be shown on this module
-
-## Developer commands
-
-- `npm install` - Install devDependencies like ESLint.
-- `npm run lint` - Run linting and formatter checks.
-- `npm run lint:fix` - Fix linting and formatter issues.
+| Option                       | Description
+| ---------------------------- | -----------
+| `parada  `                   | ID number of the bus stop.<br><br>**Required**<br>**Value type:** `Integer`<br>**Default value:** `1`
+| `consorcio`                  | Consorcio number.<br><br>**Required**<br>**Value type:** `Integer`<br>**Default value:** `7`
+| `updateInterval`             | Time (in seconds) to wait before refreshing the data from the API.<br><br>**Required**<br>**Value type:** `Integer`<br>**Default value:** `300`
 
 [mm]: https://github.com/MagicMirrorOrg/MagicMirror
